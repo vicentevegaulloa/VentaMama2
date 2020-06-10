6257ea3b3fc68258c231b39daebf5e7646d73b2d
Vicente <vnvega@uc.cl>
2020-06-10 13:04:33 -0400

    devise

--- VentaAppCarne/app/helpers/application_helper.rb
+++ VentaAppCarne/app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def admin_logged_in?
+       !current_admin.nil?
+  end
 end
--- VentaAppCarne/config/routes.rb
+++ VentaAppCarne/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :admins
   resources :sale_periods
   resources :sales
   resources :products
