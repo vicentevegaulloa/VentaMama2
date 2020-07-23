34307c5f7936f5f402239cae5a34f104d7d28f95
Vicente <vnvega@uc.cl>
2020-07-23 18:47:42 -0400

    seed

--- Otros/data.py
+++ Otros/data.py
@@ -3,19 +3,29 @@ import numpy as np
 
 df = pd.read_csv("negocio clacla.csv", ';')
 
-# print(df.head())
-# cols = df.columns.tolist()
-# for i, col in enumerate(cols):
-#     print(i, col)
+print(df.head())
+cols = df.columns.tolist()
+for i, col in enumerate(cols):
+    print(i, col)
+
+df['DIRECCION'] = df['DIRECCION'].astype(str)
+df['DIRECCION'] = df['DIRECCION'].replace(['nan', 'None'], '')
+
+df['CLIENTE'] = df['CLIENTE'].astype(str)
+df['CLIENTE'] = df['CLIENTE'].replace(['nan', 'None'], '')
+
 df['SECTOR'] = df['SECTOR'].astype(str)
 df['SECTOR'] = df['SECTOR'].replace(['nan', 'None'], '')
 print(df['SECTOR'])
 df['SECTOR'] = df['SECTOR'].replace(['LOMAS DE SAN SEBASTIAN', 'LOMAS DE SAN SEBASTIÁN'], 'LOMAS SAN SEBASTIAN')
 df['SECTOR'] = df['SECTOR'].replace(['LOMAS DE SAN ANDRÉS'], 'LOMAS DE SAN ANDRES')
 
-sectores = list(df.SECTOR.unique())
+# sectores = list(df.SECTOR.unique())
+#
+# sect_ord = [ str(sector) for sector in sectores]
+# sect_ord.sort()
+# for i in sect_ord:
+#     print(i)
 
-sect_ord = [ str(sector) for sector in sectores]
-sect_ord.sort()
-for i in sect_ord:
-    print(i)
+# clientes = df[['CLIENTE','DIRECCION', 'SECTOR']]
+# clientes.to_csv('clientes.csv', sep=';', encoding='utf-8', index=False)
--- VentaAppCarne/db/seeds.rb
+++ VentaAppCarne/db/seeds.rb
@@ -5,3 +5,72 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+productos = CSV.read(Rails.root.join("lib/assets/seed/productos.csv"), :headers=>true, :col_sep => ";")
+clientes = CSV.read(Rails.root.join("lib/assets/seed/clientes.csv"), :headers=>true, :col_sep => ";")
+
+Admin.create!(
+  email: 'culloa@arauco.cl',
+  password: '123456',
+  password_confirmation: '123456'
+)
+
+[
+  'Vendido',
+  'Entregado',
+  'Pagado'
+].each do |s|
+  State.create!(
+    name: s
+  )
+end
+
+clientes.each do |c|
+  # puts c['CLIENTE']
+  client = Client.create!(
+    name: c['CLIENTE'],
+    phone: "88888888",
+    name_phone: c['CLIENTE']
+  )
+
+  direction = Direction.create!(
+    sector: c['SECTOR'],
+    street: c['DIRECCION'],
+    client_id: client.id
+  )
+end
+puts productos
+productos.each do |p|
+  puts p['NOMBRE']
+  product = Product.create!(
+    name: p['NOMBRE'],
+    unit_price: p['UNIT PRICE'],
+    unit_metric: p['UNIT METRIC'],
+    supp_price: p['SUPP PRICE'],
+    estimated_weight: p['EST WEIGHT']
+  )
+end
+
+Client.all.each do |c|
+  num_sales = rand(1..10)
+  num_states = State.all.length
+  puts "Estados: #{num_states}"
+  num_sales.times do
+    s = Sale.create!(
+      client_id: c.id
+    )
+    s.products << Product.all.sample(1 + rand(Product.all.count))
+    s.sale_products.each do |sp|
+      sp.update(
+        cantidad: rand(1..3),
+        real_weight: rand((sp.product.estimated_weight * 0.75)..(sp.product.estimated_weight * 1.25))
+      )
+    end
+    for i in 1..rand(2..num_states)
+      puts "State: #{i}"
+      s.states << State.find(i)
+    end
+  end
+end
